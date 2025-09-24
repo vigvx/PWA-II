6fefaf8adc7cac5d39249eea2bba4112b540de46
vigvx <pedrovigards0409@gmail.com>
2025-09-24 20:01:34 -0300

    manifest.js

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{"name" : "Cartas Yu Gi OH ",
+    "short_name": "Cards",
+    "start_url": "atv-display.pedrovigar.html",
+    "display": "standalone",
+    "background_color": "#483D8B",
+    "theme_color": "#483D8B",
+    "icons": [
+        {
+            "src": "ic_launcher.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+
+        {
+            "src": "ic_launcher.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ]
+}
